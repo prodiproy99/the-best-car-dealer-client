b27d95c1d457a40c1467715317e96a33606fc130
Prodip Roy <you@example.com>
2022-05-02 18:35:01 +0600

    Navbar create

---
+++ public/__steps.js
@@ -0,0 +1,11 @@
+/**
+ * ---------------------
+ *  1. create react app
+ *  2. install react router dom
+ *  3. setup browserrouter
+ *  4. install tailwind and set up
+ *  5. install react bootstrap
+ * -----------------------
+ * 
+ * 
+*/
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Items from '../Items/Items';
+
+const Home = () => {
+    return (
+        <div> 
+            <Header></Header>
+            <Items></Items>
+        </div>
+    );
+};
+
+export default Home;
